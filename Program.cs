30f7a5cc379ba896a8d2c4c771dc99649a53876d
Leslie Fabishilaki <101106794+fabie-l@users.noreply.github.com>
2023-10-11 15:21:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace QC_EntityFramework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
